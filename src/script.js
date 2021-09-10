58a17b0361cd6bffde3de9e50f6e0c5ceee9ab57
Volos Ivan <ivan.volos1@gmail.com>
2021-09-10 16:44:17 +0300

    Added particles with a torus

--- src/script.js
+++ src/script.js
@@ -3,6 +3,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Texture Loader
+const loader = new THREE.TextureLoader()
+const cross = loader.load('./cross_blue.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -15,14 +19,38 @@ const scene = new THREE.Scene()
 // Objects
 const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
+const particlesGeometry = new THREE.BufferGeometry();
+const particlesCount = 10000;
+
+// provides x,y,z location to particles.
+const posArray = new Float32Array(particlesCount * 3);
+// xyz, xyz, xyz
+
+for(let i = 0; i < particlesCount * 3; i++){
+    posArray[i] = (Math.random() - 0.5) * (Math.random() * 5);
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3));
+
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.PointsMaterial({
+    size: 0.005,
+    color: 0x808080
+});
+
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.008,
+    map: cross,
+    transparent: true,
+    color: 0xff20ff,
+    blending: THREE.AdditiveBlending
+})
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const sphere = new THREE.Points(geometry,material);
+const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(sphere, particlesMesh);
 
 // Lights
 
@@ -77,6 +105,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(new THREE.Color('#21282a'), 1)
 
 /**
  * Animate
@@ -91,6 +120,9 @@ const tick = () =>
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
+    particlesMesh.rotation.y = .1 * elapsedTime
+    particlesMesh.rotation.z = Math.sin(.2 * elapsedTime)
+
 
     // Update Orbital Controls
     // controls.update()
